79d24550635335b89bbed8f571d5b1bc0d957153
biscuitpourqui <elibonie@hotmail.com>
2020-09-14 10:10:21 +0200

    setup index.js with reactDom.render

---
+++ src/index.js
@@ -0,0 +1,15 @@
+function App(props) {
+    return (
+        <div
+            id="app"
+        >
+            <h1>Waar zijn mijn ongeziene dieren?</h1>
+        </div>
+    )
+};
+
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root'),
+);
